289e172fd07e5fe8467a774801ed63e19bba30ca
Vladimir Leveryev <volodya.leveryev@gmail.com>
2023-03-16 12:59:14 +0900

    Fix add Bootstrap CSS

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,14 @@
+import Link from "next/link"
+
 export default () => {
-    return <div>About</div>
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1>About</h1>
+                    <Link href="/">Home</Link>
+                </div>
+            </div>
+        </div>
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
+import Link from "next/link"
+
 export default () => {
-    return <div>Index</div>
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1>Index</h1>
+                    <Link href="/about">About</Link>
+                </div>
+            </div>
+        </div>
+    )
 }
